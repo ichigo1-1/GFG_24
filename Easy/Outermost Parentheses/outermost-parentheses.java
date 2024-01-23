060aee91199d55f040a1e6554807f7bed56c174b
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-01-23 20:44:36 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Outermost Parentheses/outermost-parentheses.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String s;
+            s = br.readLine();
+
+            Solution obj = new Solution();
+            String res = obj.removeOuter(s);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static String removeOuter(String s) {
+        // code here
+        StringBuilder sb=new StringBuilder();
+        Stack<Character>st=new Stack<>();
+        for(char c:s.toCharArray())
+        {
+            if(c=='(')
+            {
+                if(st.size()>0)
+                {
+                    sb.append(c);
+                }
+                st.push(c);
+            }
+            else{
+                st.pop();
+                if(st.size()>0)
+                {
+                    sb.append(c);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
