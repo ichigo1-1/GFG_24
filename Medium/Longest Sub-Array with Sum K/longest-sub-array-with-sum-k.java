26681613508d9bfc0ccff271fa77db8f63532d57
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-02-11 22:59:59 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.java
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    int res = obj.lenOfLongSubarr(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+   
+    // Function for finding maximum and value pair
+    public static int lenOfLongSubarr (int A[], int N, int K) {
+        //Complete the function
+        //          BRUTE FORCE
+        // int ans=0;
+        // int sum;
+        // for(int i=0;i<N;i++)
+        // {
+        //     sum=0;
+        //     for(int j=i;j<N;j++)
+        //     {
+        //         sum+=A[j];
+        //         if(sum==K)
+        //         {
+        //             ans=Math.max(ans,j-i+1);
+        //         }
+        //     }
+        // }
+        // return ans;
+        HashMap<Integer,Integer>hm=new HashMap<>();
+        int ans=0;
+        int sum=0;
+        for(int i=0;i<N;i++)
+        {
+            sum+=A[i];
+            if(sum==K)
+            {
+                ans=i+1;
+            }
+            int rem=sum-K;
+            if(hm.containsKey(rem))
+            {
+                int remlength=i-hm.get(rem);
+                ans=Math.max(ans,remlength);
+            }
+            if(!hm.containsKey(sum))
+            {
+                hm.put(sum,i);
+            }
+        }
+        return ans;
+    }
+    
+    
+}
+
+
