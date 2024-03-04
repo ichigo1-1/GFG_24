1d997e1b29c8ce84d5f1a9e78335a2c9fb771ac0
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-03-04 12:47:27 +0530

    Updated Solution - GfG to GitHub

--- Easy/Bubble Sort/bubble-sort.java
+++ Easy/Bubble Sort/bubble-sort.java
@@ -8,28 +8,26 @@ import java.io.*;
 // } Driver Code Ends
 //User function Template for Java
 
-class Solution {
-    // Function to sort the array using bubble sort algorithm.
-    public static void bubbleSort(int arr[], int n) {
-        boolean swapped;
-        for (int i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // Swap arr[j] and arr[j+1]
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
+class Solution
+{
+    //Function to sort the array using bubble sort algorithm.
+	public static void bubbleSort(int arr[], int n)
+    {
+        //code here
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-1-i;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
                 }
             }
-            // If no two elements were swapped in the inner loop, then the array is already sorted
-            if (!swapped)
-                break;
         }
     }
 }
-
 
 //{ Driver Code Starts.
 
