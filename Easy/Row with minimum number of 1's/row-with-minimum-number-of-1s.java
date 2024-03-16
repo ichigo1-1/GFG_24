2f6ffe2a7860e27d4605d41965b41eba1002e2ae
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-03-16 23:14:24 +0530

    Updated Solution - GfG to GitHub

--- Easy/Row with minimum number of 1's/row-with-minimum-number-of-1s.java
+++ Easy/Row with minimum number of 1's/row-with-minimum-number-of-1s.java
@@ -34,23 +34,44 @@ class GFG {
 class Solution {
     int minRow(int N, int M, int A[][]) {
         // code here
-        int minOnes = Integer.MAX_VALUE;
-        int minIndex = -1;
+        // int minOnes = Integer.MAX_VALUE;
+        // int minIndex = -1;
 
-        for (int i = 0; i < N; i++) {
-            int onesCount = 0;
-            for (int j = 0; j < M; j++) {
-                if (A[i][j] == 1) {
-                    onesCount++;
+        // for (int i = 0; i < N; i++) {
+        //     int onesCount = 0;
+        //     for (int j = 0; j < M; j++) {
+        //         if (A[i][j] == 1) {
+        //             onesCount++;
+        //         }
+        //     }
+
+        //     if (onesCount < minOnes || (onesCount == minOnes && i < minIndex)) {
+        //         minOnes = onesCount;
+        //         minIndex = i;
+        //     }
+        // }
+
+        // return minIndex + 1;
+        
+    int max_ones=Integer.MAX_VALUE;
+    int index=-1;
+        for(int i=0;i<N;i++)
+        {
+            int count=0;
+            for(int j=0;j<M;j++)
+            {
+                if(A[i][j]==1)
+                {
+                    count++;
                 }
             }
-
-            if (onesCount < minOnes || (onesCount == minOnes && i < minIndex)) {
-                minOnes = onesCount;
-                minIndex = i;
+            if(count<max_ones)
+            {
+                max_ones=count;
+                index=i;
             }
+            
         }
-
-        return minIndex + 1;
+        return index+1;
     }
 };
