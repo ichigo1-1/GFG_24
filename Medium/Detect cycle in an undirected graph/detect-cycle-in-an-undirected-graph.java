b43fc3adf746187cc5c66af5a4af9d89800f3250
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-05-23 01:58:01 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.java
+++ Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.java
@@ -30,52 +30,48 @@ class GFG {
     }
 }
 // } Driver Code Ends
+class Pair {
+    int f, s;
 
-
+    Pair(int ff, int ss) {
+        f = ff;
+        s = ss;
+    }
+}
 class Solution {
-    // Function to detect cycle in an undirected graph.
-     public boolean dfs(int start,int parent,ArrayList<ArrayList<Integer>> adj,int[]vis)
-    {
-        vis[start]=1;
-        //now look for adjacent nodes;
-        for(int it:adj.get(start))
-        {
-            if(vis[it]==0)
-            {
-                if(dfs(it,start,adj,vis)==true)
-                {
+    // Function to detect cycle in an undirected graph using BFS
+    public boolean bfs(int src, boolean[] vis, ArrayList<ArrayList<Integer>> adj) {
+        vis[src] = true;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(src, -1));
+
+        while (!q.isEmpty()) {
+            int currentnode = q.peek().f;
+            int parent = q.peek().s;
+            q.poll();
+
+            for (int adjacent : adj.get(currentnode)) {
+                if (!vis[adjacent]) {
+                    vis[adjacent] = true;
+                    q.add(new Pair(adjacent, currentnode));
+                } else if (parent != adjacent) {
                     return true;
                 }
             }
-            //agar visited hai and is visited by someone which is not the parent
-            else if(it!=parent){
-                
-                
-                    return true;
-                    
-                }
-            
         }
-        
-        //while going back remove from path and return false
-
         return false;
     }
+
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
-        // Code here
-         int[]vis=new int[V];
-       
-        for(int i=0;i<V;i++)
-        {
-            if(vis[i]==0)
-            {
-                if(dfs(i,-1,adj,vis)==true)
-                {
+        boolean[] vis = new boolean[V];
+
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                if (bfs(i, vis, adj)) {
                     return true;
                 }
             }
         }
         return false;
-        
     }
 }
