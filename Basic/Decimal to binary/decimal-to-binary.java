25ffc309c2d29558fcc43b30c56e60d003431da9
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-06-13 16:51:59 +0530

    Updated Solution - GfG to GitHub

--- Basic/Decimal to binary/decimal-to-binary.java
+++ Basic/Decimal to binary/decimal-to-binary.java
@@ -24,16 +24,29 @@ class GFG{
 class Solution{
 	void toBinary(int N) {
 		//Write your code here
-		int rem = 0 , i = 0;
-        ArrayList<Integer> arr = new ArrayList<>();
-        while(N > 0){
-            rem = N%2;
-            arr.add(rem);
-            N = N/2;
-        }
+// 		int rem = 0 , i = 0;
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         while(N > 0){
+//             rem = N%2;
+//             arr.add(rem);
+//             N = N/2;
+//         }
         
-        for(int j = arr.size()-1 ; j >= 0 ; j--){
-            System.out.print(arr.get(j));
-        }
+//         for(int j = arr.size()-1 ; j >= 0 ; j--){
+//             System.out.print(arr.get(j));
+//         }
+int rem=0;
+int i=0;
+ArrayList<Integer>arr=new ArrayList<>();
+while(N>0)
+{
+    rem=N%2;
+    arr.add(rem);
+    N/=2;
+}
+for(int j=arr.size()-1;j>=0;j--)
+{
+    System.out.print(arr.get(j));
+}
     }
 	}
