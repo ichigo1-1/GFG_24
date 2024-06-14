5502a909583d2ab74db52b7a4c813352e425298c
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-06-15 00:06:24 +0530

    Updated Solution - GfG to GitHub

--- Easy/Leaders in an array/leaders-in-an-array.java
+++ Easy/Leaders in an array/leaders-in-an-array.java
@@ -4,55 +4,69 @@ import java.util.*;
 import java.util.stream.*;
 
 class Array {
-    
-	public static void main (String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		PrintWriter ot = new PrintWriter(System.out);
-		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
-		
-		while(t-->0){
-		    
-		    //input size of array
-		    int n = Integer.parseInt(br.readLine().trim());
-		    int arr[] = new int[n];
-		    String inputLine[] = br.readLine().trim().split(" ");
-		    
-		    //inserting elements in the array
-		    for(int i=0; i<n; i++){
-		        arr[i] = Integer.parseInt(inputLine[i]);
-		    }
-		    
-		    Solution obj = new Solution();
-		    
-		    StringBuffer str = new StringBuffer();
-		    ArrayList<Integer> res = new ArrayList<Integer>();
-		  
-		    //calling leaders() function
-		    res = obj.leaders(arr, n);
-		    
 
-		    for(int i=0; i<res.size(); i++){
-		        ot.print(res.get(i)+" ");
-		    }
-		    
-		    ot.println();
-		}
-		ot.close();
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter ot = new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+
+        while (t-- > 0) {
+
+            // input size of array
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+
+            // inserting elements in the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            Solution obj = new Solution();
+
+            StringBuffer str = new StringBuffer();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+
+            // calling leaders() function
+            res = obj.leaders(n, arr);
+
+            for (int i = 0; i < res.size(); i++) {
+                ot.print(res.get(i) + " ");
+            }
+
+            ot.println();
+        }
+        ot.close();
+    }
 }
 
 // } Driver Code Ends
 
 
-
-// Java program to demonstrate 
-// How to sort ArrayList in descending order 
-
-class Solution{
-    //Function to find the leaders in the array.
-    static ArrayList<Integer> leaders(int arr[], int n){
+class Solution {
+    // Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int n, int arr[]) {
         // Your code here
+        // for(int i=0;i<n-1;i++)
+        // {
+        //     boolean flag=false;
+        //     for(int j=i+1;j<n;j++)
+        //     {
+        //         if(arr[i]<arr[j])
+        //         {
+        //             flag=true;
+        //             break;
+        //         }
+                
+        //     }
+        //     if(flag==false)
+        //     {
+        //         arrr.add(arr[i]);
+        //     }
+        // }
+        // arrr.add(arr[n-1]);
+        // return arrr;
+        
         ArrayList<Integer>ls=new ArrayList<>();
         //always add the last
         ls.add(arr[n-1]);
