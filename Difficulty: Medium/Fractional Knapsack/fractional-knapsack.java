ca0de4a065dd6c1a550e23940611b646d1f52b0b
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-07-09 13:19:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.java
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Item {
+    int value, weight;
+
+    Item(int x, int y) {
+        this.value = x;
+        this.weight = y;
+    }
+}
+
+class GfG {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int w = Integer.parseInt(inputLine[1]);
+            Item[] arr = new Item[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0, k = 0; i < n; i++) {
+                arr[i] = new Item(Integer.parseInt(inputLine[k++]),
+                                  Integer.parseInt(inputLine[k++]));
+            }
+            System.out.println(
+                String.format("%.6f", new Solution().fractionalKnapsack(w, arr, n)));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Item {
+    int value, weight;
+    Item(int x, int y){
+        this.value = x;
+        this.weight = y;
+    }
+}
+*/
+class comp implements Comparator<Item>
+{
+    @Override
+    public int compare(Item a,Item b)
+    {
+        //sorting in descending order
+        double r1=(double)(a.value)/(double)(a.weight);
+        double r2=(double)(b.value)/(double)(b.weight);
+        if(r1<r2)
+        {
+            return 1;
+        }
+        else if(r1>r2)
+        {
+            return -1;
+        }
+        else{
+            return 0;
+        }
+        
+        
+    }
+}
+class Solution {
+    // Function to get the maximum total value in the knapsack.
+    double fractionalKnapsack(int w, Item arr[], int n) {
+        // Your code here
+        Arrays.sort(arr,new comp());
+        int curweight=0;
+        double ans=0.0;
+        for(int i=0;i<n;i++)
+        {
+            if(curweight+arr[i].weight<=w)
+            {
+                curweight+=arr[i].weight;
+                ans+=arr[i].value;
+            }
+            else{
+                int remaining=w-curweight;
+                ans+=((double)arr[i].value/(double)arr[i].weight)*(double)remaining;
+                break;
+            }
+        }
+         return ans;
+    }
+   
+}
