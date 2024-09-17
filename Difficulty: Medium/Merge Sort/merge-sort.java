77b1e01d8456389dc325385281cda3722e4a4e1e
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-09-18 00:22:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.java
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -55,45 +55,58 @@ class Merge_Sort
 // } Driver Code Ends
 
 
-class Solution {
-    void merge(int arr[], int l, int m, int r) {
-        // Your code here
-        ArrayList<Integer> temp = new ArrayList<>();
-        int left = l;
-        int right = m + 1;
-        
-        while (left <= m && right <= r) {
-            if (arr[left] <= arr[right]) {
-                temp.add(arr[left]);
-                left++;
-            } else {
-                temp.add(arr[right]);
-                right++;
+class Solution
+{
+    void merge(int arr[], int l, int m, int r)
+    {
+         // Your code 
+        ArrayList<Integer>temp=new ArrayList<>();
+        int left=l;
+        int right=m+1;
+        while(left<=m && right<=r)
+        {
+            if(arr[left]<=arr[right])
+            {
+                temp.add(arr[left++]);
+            }
+            else{
+                temp.add(arr[right++]);
             }
         }
-        
-        while (left <= m) {
-            temp.add(arr[left]);
-            left++;
+        while(left<=m)
+        {
+            temp.add(arr[left++]);
+        }
+        while(right<=r)
+        {
+            temp.add(arr[right++]);
+        }
+        //put back
+        for(int i=l;i<=r;i++)
+        {
+            arr[i]=temp.get(i-l);
         }
         
-        while (right <= r) {
-            temp.add(arr[right]);
-            right++;
-        }
-        
-        for (int i = l; i <= r; i++) {
-            arr[i] = temp.get(i - l);
-        }
-    }
 
-    void mergeSort(int arr[], int l, int r) {
-        if (l == r) {
+    }
+    void mergeSort2(int[]arr,int l,int r)
+    {
+        if(l>=r)
+        {
+            return;
         }
-            int mid = (l + r) / 2;
-            mergeSort(arr, l, mid);
-            mergeSort(arr, mid + 1, r);
-            merge(arr, l, mid, r);
-        }
+        int mid=(l+r)/2;
+        mergeSort2(arr,l,mid);
+        mergeSort2(arr,mid+1,r);
+        merge(arr,l,mid,r);
+        
+    }
+    void mergeSort(int arr[], int l, int r)
+    {
+        //code here
+        //base case
+        int n=arr.length;
+        mergeSort2(arr,0,n-1);
+        
     }
 }
