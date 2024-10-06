00a77eb2a58a176e6c2a437b1dbe865045bf5405
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-10-06 23:29:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Kth from End of Linked List/kth-from-end-of-linked-list.java
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+
+// } Driver Code Ends
+/* Structure of node
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+} */
+
+class Solution {
+
+    // Function to find the data of kth node from
+    // the end of a linked list.
+    int getKthFromLast(Node head, int k) {
+        // Your code here
+        if(head==null)
+        {
+            return -1;
+        }
+        Node temp=head;
+        int c=0;
+        while(temp!=null)
+        {
+            temp=temp.next;
+            c++;
+        }
+        if(k>c)
+        {
+            return -1;
+        }
+        int tomove=c-k;
+        temp=head;
+        while(tomove>0)
+        {
+            temp=temp.next;
+            tomove--;
+        }
+        return temp.data;
+        
+    }
+}
+
+
+//{ Driver Code Starts.
+
+public class GFG {
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            List<Integer> arr = new ArrayList<>();
+            String input = br.readLine().trim();
+            if (!input.isEmpty()) {
+                String[] numbers = input.split("\\s+");
+                for (String num : numbers) {
+                    if (!num.isEmpty()) {
+                        arr.add(Integer.parseInt(num));
+                    }
+                }
+            }
+
+            int x = Integer.parseInt(br.readLine().trim());
+
+            Node head = null;
+            if (!arr.isEmpty()) {
+                head = new Node(arr.get(0));
+                Node tail = head;
+                for (int i = 1; i < arr.size(); ++i) {
+                    tail.next = new Node(arr.get(i));
+                    tail = tail.next;
+                }
+            }
+
+            Solution g = new Solution();
+            // System.out.println(k);
+            System.out.println(g.getKthFromLast(head, x));
+        }
+    }
+}
+
+// } Driver Code Ends
