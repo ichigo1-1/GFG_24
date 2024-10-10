e660512383991bde8cca5d0b51b3e71dae88895c
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-10-11 01:52:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reorganize The Array/reorganize-the-array.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+// } Driver Code Ends
+class Solution {
+    public List<Integer> rearrange(List<Integer> arr) {
+        // Code here
+         for(int i=0;i<arr.size();i++)
+        {
+            if(arr.get(i)<arr.size())
+            {
+                //swap
+                while (arr.get(i) >= 0 && arr.get(i) < arr.size() && arr.get(i) != i) {
+                int correctIndex = arr.get(i);
+                // Swap the current value to its correct position
+                int temp = arr.get(i);
+                arr.set(i, arr.get(correctIndex));
+                arr.set(correctIndex, temp);
+            }
+            }
+        }
+         for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) != i) {
+                arr.set(i, -1);
+            }
+        }
+        return arr;
+    }
+}
+
+//{ Driver Code Starts.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+        Solution solution = new Solution();
+
+        while (t-- > 0) {
+            String input = scanner.nextLine();
+            String[] inputArr = input.split("\\s+");
+            List<Integer> arr = new ArrayList<>();
+            for (String s : inputArr) {
+                arr.add(Integer.parseInt(s));
+            }
+
+            List<Integer> ans = solution.rearrange(arr);
+
+            for (int num : ans) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
+// } Driver Code Ends
